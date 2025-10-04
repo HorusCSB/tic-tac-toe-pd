0a8fdcdd70b38ea58a90f55436778bdef256d19d
horus <horus.cristian@gmail.com>
2025-10-03 21:14:28 -0300

    feat: create tic-tac-toe comunication

--- client.py
+++ client.py
@@ -1,7 +1,44 @@
 import xmlrpc.client
 
+def printBoard(board):
+    print()
+    for row in board:
+        print(row)
+    print()
+
 if __name__ == '__main__':
-    client  = xmlrpc.client.ServerProxy("http://localhost:8080")
-    num = int(input('Digite um numero:'))
-    snum = client.sucessor(num)
-    print('sucessor de ',num,' = ',snum)
+    client = xmlrpc.client.ServerProxy("http://localhost:8080")
+    name = input("insira seu nome:")
+    playerId, playerName = client.connect(name)
+    
+    if playerId == -1:
+        print(playerName)
+        exit()
+        
+    print(f"Você é: {playerName}")
+
+    while True:
+        vencedor = client.checkWinStatus()
+        if vencedor:
+            print(vencedor)
+            printBoard(client.getBoard())
+            break
+        
+        currentPlayer = client.getCurrentTurn()
+        printBoard(client.getBoard())
+        print(f"Vez de: {currentPlayer}")
+        
+        if currentPlayer == playerName:
+            while True:
+                try:
+                    jogada = input("Digite linha,coluna (ex: 0,2): ")
+                    linha, coluna = map(int, jogada.split(","))
+
+                    playResponse = client.playerTurn(linha, coluna, playerId)
+
+                    if "sucesso" in playResponse:
+                        break
+                except:
+                    exit()
+
+
--- jogo-velha.py
+++ jogo-velha.py
@@ -1,108 +1,119 @@
-player = ['','']
+import xmlrpc.server
 
-currentPlayer = 'name'
+##### ----------- variáveis globais do jogo (jogadores, jogador atual, status do jogo, espaços em branco, tabuleiro, quantidade de jogadores conectados, jogadas permitidas no terminal)
+player = ['', '']
+currentPlayerIndex = 0
 win = False
 emptySpaces = 9
+board = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None]
+]
+playersConnected = 0
 
-board = []
+allowedPlays = [0, 1, 2]
 
-allowedPlays = [0,1,2]
+##### ----------- regras do jogo da velha
+def getBoard():
+    return board
 
-def createEmptyTicTacToe():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None]
-    ]
-
-def start():
-    global player
-    global currentPlayer
-    global board
-    global emptySpaces
-    global win
-
-    player[0] = input("insira o nome do jogador 1:")
-    player[1] = input("insira o nome do jogador 2:")
-    currentPlayer = player[0]
-    emptySpaces = 9
-    win = False
-
-    board = createEmptyTicTacToe()
+def getCurrentTurn():
+    if playersConnected < 2:
+        return "aguardando o segundo jogador..."
+    if win:
+        return "fim"
+    return player[currentPlayerIndex]
 
 def checkWin():
-    global board
-    global win
-    global emptySpaces
-    global currentPlayer
-    global player
-    #cabou espaço empatou
-    if emptySpaces == 0: 
+    global win, emptySpaces
+
+    if emptySpaces == 0 and not win:
         win = 'tie'
+        print("empate!")
         return
-    #diagonal
-    if (board[0][0] == board[1][1] == board[2][2]) and board[0][0] != None:
+
+    # diagonal 1
+    if (board[0][0] == board[1][1] == board[2][2]) and board[0][0] is not None:
         win = True
+        print(f"vitória: {board[0][0]}")
         return
-    #linhas
-    for n in board:
-        if (n[0] == n[1] == n[2]) and n[0] != None:
+
+    # diagonal 2
+    if (board[0][2] == board[1][1] == board[2][0]) and board[0][2] is not None:
+        win = True
+        print(f"vitória: {board[0][2]}")
+        return
+
+    # linhas
+    for x in board:
+        if (x[0] == x[1] == x[2]) and x[0] is not None:
             win = True
+            print(f"vitória: {x[0]}")
             return
-    #colunas
-    for n in range(3):
-        if (board[0][n] == board[1][n] == board[2][n]) and board[0][n] != None:
+
+    # colunas
+    for y in range(3):
+        if (board[0][y] == board[1][y] == board[2][y]) and board[0][y] is not None:
             win = True
+            print(f"vitória: {board[0][y]}")
             return
-    currentPlayer = player[1] if currentPlayer == player[0] else player[0]
 
-def printBoard():
-    print()
-    print(board[0])
-    print(board[1])
-    print(board[2])
 
-def playerTurn():
-    global currentPlayer
-    global emptySpaces
-    global board
-    
-    print()
-    print('Jogador da vez:', currentPlayer)
-    print()
+def playerTurn(linha, coluna, playerId):
+    global board, emptySpaces, currentPlayerIndex, player, playersConnected, allowedPlays
 
-    while True:
-        linha = input("digite a linha (0,1,2):")
-        coluna = input("digite a coluna (0,1,2):")
-        try:
-            linha = int(linha)
-            coluna = int(coluna)
-        except ValueError:
-            print("Digite apenas números inteiros!")
-            continue
-        if linha not in allowedPlays or coluna not in allowedPlays:
-            print("Por favor, escolha um valor válido!")
-        if (board[linha][coluna] != None):
-            print("Esta posição já foi preenchida, tente novamente!")
-        else:
-            break
+    print(f"jogada: {player[playerId]}, ({linha}, {coluna})")
 
-    board[linha][coluna] = currentPlayer
+    if linha not in allowedPlays or coluna not in allowedPlays:
+        print("fora do tabuleiro.")
+        return "jgoda inválida! valores permitidos: [0, 1, 2]."
+
+    if board[linha][coluna] is not None:
+        print("posição ocupada")
+        return "jogada inválida, casa preenchida"
+
+    board[linha][coluna] = player[playerId]
     emptySpaces -= 1
     checkWin()
-    printBoard()
 
-
-def main():
-    start()
-    while win == False and win != 'tie':
-        playerTurn()
-
-    print()
-    if(win == 'tie'):
-        print("Empate!")
+    if not win:
+        currentPlayerIndex = 1 - currentPlayerIndex
+        print(f"turno de: {player[currentPlayerIndex]}.")
     else:
-        print(currentPlayer,"venceu!")
-if __name__ == "__main__":
-    main()
+        print("----------------")
+        print("fim de jogo")
 
+    return "sucesso"
+
+def checkWinStatus():
+    if win == 'tie':
+        return "empate!"
+    elif win:
+        return f"{player[currentPlayerIndex]} venceu!"
+    return False
+
+##### ----------- operações de servidor
+def connect(name):
+    global playersConnected
+    if playersConnected < 2:
+        playerId = playersConnected
+        playersConnected += 1
+        player[playerId] = name
+        print(f"jogador conectado: {player[playerId]}, id: {playerId}")
+        return playerId, f"{player[playerId]}"
+    return -1, "sala cheia"
+
+if __name__ == '__main__':
+    # Inicializa os nomes dos jogadores para exibição
+    server = xmlrpc.server.SimpleXMLRPCServer(('localhost', 8080), allow_none=True)
+
+    print("rodando")
+
+    server.register_function(connect)
+    server.register_function(getBoard)
+    server.register_function(getCurrentTurn)
+    server.register_function(playerTurn)
+    server.register_function(checkWinStatus)
+
+    server.serve_forever()
